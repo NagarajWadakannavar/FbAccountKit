591a2fb075e3f44c5c5ff74a988695c18f708a8c
Nagaraj M W <nagarajw@ndtv.com>
2016-06-26 16:39:20 +0530

    Initial commit

---
+++ app/src/main/java/AccountKitApplication.java
@@ -0,0 +1,15 @@
+import android.app.Application;
+
+import com.facebook.accountkit.AccountKit;
+
+/**
+ * Created by NAGARAJ on 6/26/2016.
+ */
+public class AccountKitApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AccountKit.initialize(getApplicationContext());
+
+    }
+}
